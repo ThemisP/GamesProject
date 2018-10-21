d5ccc8e566c58cc98abd4f3d3b94c726d03f1454
Themis <tp16381@bristol.ac.uk>
2018-10-21 21:35:27 +0100

    Basic movement and camera follow

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 6f;
+	Vector3 movement;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	// Use this for initialization
+	void Awake () {
+		floorMask = LayerMask.GetMask("Floor");
+		playerRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+
+		Move(h,v);
+		Turning();
+	}
+
+	void Move(float h, float v){
+		movement.Set(h, 0f, v);
+		//Normalise the movement vector to make it proportional to the speed per second
+		//Deltatime is the step for the game timer
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition(transform.position + movement);
+	}
+
+	void Turning(){
+		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		RaycastHit floorHit;
+
+		if(Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)){
+			Vector3 playerToMouse = floorHit.point - transform.position;
+
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+			playerRigidbody.MoveRotation(newRotation);
+		}
+	}
+}
