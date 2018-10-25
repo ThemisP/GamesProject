0bcdb856a94e46dbd12da49454a22f03df10095a
Themis <themispapathemistocleous@gmail.com>
2018-10-25 21:25:18 +0100

    First try at Photon

---
+++ Assets/Resources/Scripts/PhotonNetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonNetworkManager : Photon.PunBehaviour {
+
+	[SerializeField] private Text connectText;
+	[SerializeField] private GameObject player;
+	[SerializeField] private Transform spawnPoint;
+	[SerializeField] private GameObject lobbyCammera;
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+
+	public virtual void OnJoinedLobby(){
+		Debug.Log("Connected to lobby");
+
+		PhotonNetwork.JoinOrCreateRoom("Room1", null, null);
+	}
+
+	public virtual void OnJoinedRoom(){
+		PhotonNetwork.Instantiate(player.name, spawnPoint.position, spawnPoint.rotation, 0);
+		lobbyCammera.GetComponent<CameraFollow>().target = player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		connectText.text = PhotonNetwork.connectionStateDetailed.ToString();
+	}
+}
