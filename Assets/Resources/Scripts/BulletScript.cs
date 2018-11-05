39f45f78963e4400358cda74c2db2a632bfd0113
Themis <themispapathemistocleous@gmail.com>
2018-11-05 12:48:45 +0000

    bullet work

---
+++ Assets/Resources/Scripts/BulletScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : Photon.MonoBehaviour {
+
+
+	float lifeTime = 2f;
+	// Update is called once per frame
+	void Update () {
+		lifeTime -= Time.deltaTime;
+		if(lifeTime<0)
+			PhotonNetwork.Destroy(GetComponent<PhotonView>());
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		PhotonNetwork.Destroy(GetComponent<PhotonView>());
+	}
+}
--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -62,11 +62,10 @@ public class PlayerController : Photon.MonoBehaviour {
 	void Fire(bool fire){
 		if(fire){
 			if(lastShootTime+fireRate<Time.fixedTime){
-				GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+				GameObject bullet = PhotonNetwork.Instantiate(bulletPrefab.name, bulletSpawn.position, bulletSpawn.rotation, 0);
 
-				bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6f;
+				bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 10f;
 
-				Destroy(bullet, 2.0f);
 				lastShootTime = Time.fixedTime;
 			}
 		} 
