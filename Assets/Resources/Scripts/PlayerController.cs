7af6c8c41c1d0a0172bd02ceaea875f77c486167
Themis <themispapathemistocleous@gmail.com>
2018-11-05 11:33:08 +0000

    Working on fireing bullets

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
+	[SerializeField] private GameObject bulletPrefab;
+	[SerializeField] private Transform bulletSpawn;
 	[SerializeField] private float speed = 6f;
 	Vector3 movement;
 	Rigidbody playerRigidbody;
 	int floorMask;
 	float camRayLength = 100f;
+
+	float lastShootTime = 0;
+	[SerializeField] private float fireRate = 2f;
 
 	// Use this for initialization
 	void Awake () {
@@ -18,11 +23,13 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");
+		float h = Input.GetAxisRaw("Horizontal");// a and d keys
+		float v = Input.GetAxisRaw("Vertical"); // w and s keys
+		bool fire = Input.GetMouseButton(0);//pressed primary mouse button
 
 		Move(h,v);
 		Turning();
+		Fire(fire);
 	}
 
 	void Move(float h, float v){
@@ -47,5 +54,18 @@ public class PlayerMovement : MonoBehaviour {
 
 			playerRigidbody.MoveRotation(newRotation);
 		}
+	}
+
+	void Fire(bool fire){
+		if(fire){
+			if(lastShootTime+fireRate<Time.fixedTime){
+				GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+				bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6f;
+
+				Destroy(bullet, 2.0f);
+				lastShootTime = Time.fixedTime;
+			}
+		} 
 	}
 }
