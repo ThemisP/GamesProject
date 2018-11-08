9252a1f510771ea647d8b6350c12e7a62e99bf14
Themis <themispapathemistocleous@gmail.com>
2018-11-08 12:43:30 +0000

    UI health bar

---
+++ Assets/Resources/Scripts/Player/PlayerData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerData : Photon.MonoBehaviour {
+
+	[SerializeField] private float maxHealth = 100f;
+	public Slider healthSlider;
+	private float currentHealth;
+
+	private int kills = 0, assists = 0, deaths = 0;
+
+	private int skillPoints = 0;
+
+	private PhotonView photonView;
+	void Start()
+	{
+		currentHealth = maxHealth;
+		photonView = GetComponent<PhotonView>();
+	}
+
+	private void FixedUpdate() {
+		if(!photonView.isMine) return;
+
+		
+	}
+}
