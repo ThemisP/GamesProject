7fba16b88ebcb01763054ff176dc55641bcc0156
Themis <themispapathemistocleous@gmail.com>
2018-12-12 19:59:39 +0000

    Create and Join room.
    Logic towards joining a friend and entering the game as a team.

---
+++ Assets/PlayerNetwork.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour {
+
+	private string username;
+
+	void Awake(){
+		DontDestroyOnLoad(this);
+	}
+
+	public void setUsername(string name){
+		this.username = name;
+	}
+
+	public string getUsername(){
+		return username;
+	}
+}
--- Assets/Resources/Scripts/Network/MainMenu.cs
+++ Assets/Resources/Scripts/Network/MainMenu.cs
@@ -5,30 +5,99 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : Photon.PunBehaviour {
 
-	private GameObject usernameScreen;
-	private GameObject roomJoinCreateScreen;
-	private TextMeshProUGUI usernameInput;
-	private string username;
+	[SerializeField] private GameObject usernameScreenMenu;
+	[SerializeField] private GameObject MainScreenMenu;
+	[SerializeField] private GameObject JoinRoomMenu;
+	[SerializeField] private GameObject CreateRoomMenu;
 
-	void Start(){
-		//setting up starting ui
-		usernameScreen = GameObject.Find("ChooseUsername");
-		if(usernameScreen==null) Debug.Log("username screen not found");
-		roomJoinCreateScreen = GameObject.Find("Main");
-		if(usernameScreen==null) Debug.Log("room join or create screen not found");
-		usernameScreen.SetActive(true);
-		roomJoinCreateScreen.SetActive(false);
+	[SerializeField] private GameObject LobbyRoomMenu;
+	[SerializeField] private TMP_InputField usernameInput;
+	[SerializeField] private TMP_InputField createRoomName;
+	[SerializeField] private TMP_InputField joinRoomName;
+	[SerializeField] private TMP_Text usernameDisplay;
+	[SerializeField] private TMP_Text LobbyTitle;
+	[SerializeField] private TMP_Text player1Display;
+	[SerializeField] private TMP_Text player2Display;
 
-		//getting necessary fields
-		usernameInput = GameObject.Find("UsernameText").GetComponent<TextMeshProUGUI>();
-		if(usernameInput==null) Debug.Log("usernameInput not found");
+	[SerializeField] private Button joinMainGameFromLobbyButton;
+
+	[SerializeField] private PlayerNetwork playerNetwork;
+
+
+	void Awake(){
+		usernameScreenMenu.SetActive(true);
+		MainScreenMenu.SetActive(false);
+		JoinRoomMenu.SetActive(false);
+		CreateRoomMenu.SetActive(false);
+		LobbyRoomMenu.SetActive(false);
+
+		PhotonNetwork.ConnectUsingSettings("v1");
 	}
 
-	void ChooseUsername(){
-		if(usernameInput.text == null) Debug.Log("Not inputed username");
-		username = usernameInput.text;
+	public void ChooseUsername(){
+		string name = usernameInput.text;
+		if(string.IsNullOrEmpty(name)) {
+			Debug.Log("Field is empty");
+			return;
+		}
+		playerNetwork.setUsername(name);
+		PhotonNetwork.playerName  = name;
+		
+		usernameScreenMenu.SetActive(false);
+		MainScreenMenu.SetActive(true);
+		JoinRoomMenu.SetActive(false);
+		CreateRoomMenu.SetActive(false);
+		LobbyRoomMenu.SetActive(false);
+		usernameDisplay.text = name;
+	}
+
+	public void CreateRoom(){
+		string name = createRoomName.text;
+		if(string.IsNullOrEmpty(name)) {
+			Debug.Log("Username field empty");
+			return;
+		}
+		RoomOptions roomOptions = new RoomOptions();
+		roomOptions.MaxPlayers = 2;
+		PhotonNetwork.CreateRoom(name, roomOptions, TypedLobby.Default);		
+	}
+
+	virtual public void OnCreatedRoom(){
+		Debug.Log("Created Room");
+	}
+	
+	virtual public void OnPhotonCreateRoomFailed(object[] codeAndMsg){
+		Debug.Log(codeAndMsg[0]);
+	}
+
+	public void JoinRoom(){
+		string name = joinRoomName.text;
+		PhotonNetwork.JoinRoom(name);
+	}
+
+	virtual public void OnJoinedRoom(){
+			usernameScreenMenu.SetActive(false);
+			MainScreenMenu.SetActive(false);
+			JoinRoomMenu.SetActive(false);
+			CreateRoomMenu.SetActive(false);
+			LobbyRoomMenu.SetActive(true);
+			if(PhotonNetwork.otherPlayers.Length <1){
+				joinMainGameFromLobbyButton.interactable = true;
+				player1Display.text = PhotonNetwork.playerName;
+				LobbyTitle.text = "(" + PhotonNetwork.room.Name + ") Lobby";
+			} else {
+				joinMainGameFromLobbyButton.interactable = false;
+				player1Display.text = PhotonNetwork.otherPlayers[0].NickName;
+				player2Display.text = PhotonNetwork.playerName;
+				LobbyTitle.text = "(" + PhotonNetwork.room.Name + ") Lobby";
+			}
+			
+	}
+
+	virtual public void OnPhotonJoinRoomFailed(object[] codeAndMsg){
+		Debug.Log(codeAndMsg[0]);
 	}
 
 
