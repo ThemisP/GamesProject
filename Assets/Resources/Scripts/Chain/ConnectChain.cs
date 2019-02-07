2c3db29914831ff532c84a3d24bad9ee824f45e9
aSpikyOreo <lm16154@my.bristol.ac.uk>
2019-02-07 18:56:45 +0000

    TeamPrefab made to model CO-OP chain (STATIC ONLY) --> WIP

---
+++ Assets/Resources/Scripts/Chain/ConnectChain.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectChain : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        GetComponent<CharacterJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>(); 
+    }
+}
+
