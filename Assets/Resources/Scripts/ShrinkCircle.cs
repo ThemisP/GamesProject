51ea5d2df2b53758eeae301c97101cf6344fda31
Lucian Carp <lc16929@bristol.ac.uk>
2019-02-13 22:13:46 +0000

    Add Circle shrink(not ready)

---
+++ Assets/Resources/Scripts/ShrinkCircle.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkCircle : MonoBehaviour
+{   
+    [Range(0, 360)]
+	public int Segments;
+    [Range(0, 5000)]
+    public float Radius;
+    public float SpeedShrinking;
+    public GameObject Circle;
+    public bool Shrinking;
+
+    #region Private Members
+	private RenderCircleLine circle;
+	private LineRenderer renderer;
+	#endregion
+
+    void Start()
+    {   
+        renderer = gameObject.GetComponent<LineRenderer>();
+        circle = new RenderCircleLine(ref renderer, Segments, Radius, Radius);
+        Circle = GameObject.FindGameObjectWithTag("Circle");
+    }
+
+    void Update ()
+    {
+        if(Time.time > 2f)
+        {
+            Shrinking = true;
+        }
+
+        if(Shrinking)
+        {
+            Radius = Mathf.Lerp(Radius, 0, Time.deltaTime * SpeedShrinking);
+            circle.Draw(Segments, Radius, Radius);
+            Circle.transform.localScale = new Vector3 (Radius, 1, Radius);
+        }
+    }
+}
